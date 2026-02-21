085f3841ec6286a8581a6891ff38caa61484526b
Ramses Garcia <bryan.garcia.duran@gmail.com>
2026-02-21 11:45:17 -0600

    Initial Commit

---
+++ MDI_113_CH4_class_work Watch App/MDI_113_CH4_class_workApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDI_113_CH4_class_workApp.swift
+//  MDI_113_CH4_class_work Watch App
+//
+//  Created by Ramses Garcia on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MDI_113_CH4_class_work_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
